a30408b6d2bace5f5636ad9ffa2853fa115e1711
aurelio mont <aurelio.mont25@gmail.com>
2024-07-30 15:33:31 -0600

    Add requirements

---
+++ products/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    max_quantity = models.IntegerField()
+    min_quantity = models.IntegerField()
+    stock = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image_url = models.CharField(max_length=2083)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
